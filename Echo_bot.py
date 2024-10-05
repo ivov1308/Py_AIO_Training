7e714f0fbf6a4a96c6e453b23eb9eb13563335b9
Ivan Ovchinikov <ivan13air@mail.ru>
2024-10-05 07:15:05 +0300

    Echo without decorators

--- Echo_bot.py
+++ Echo_bot.py
@@ -10,13 +10,11 @@ dp = Dispatcher()
 
 
 # Этот хэндлер будет срабатывать на команду "/start"
-@dp.message(Command(commands=["start"]))
 async def process_start_command(message: Message):
     await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
 
 
 # Этот хэндлер будет срабатывать на команду "/help"
-@dp.message(Command(commands=["help"]))
 async def process_help_command(message: Message):
     await message.answer(
         'Напиши мне что-нибудь и в ответ '
@@ -26,11 +24,15 @@ async def process_help_command(message: Message):
 
 # Этот хэндлер будет срабатывать на любые текстовые сообщения
 # кроме команд "/start" и "/help"
-@dp.message()
 async def send_echo(message: Message):
     if message.text is not None:
         await message.reply(text=message.text)
 
+
+# Регистрируем хэндлеры
+dp.message.register(process_start_command, Command(commands='start'))
+dp.message.register(process_help_command, Command(commands='help'))
+dp.message.register(send_echo)
 
 if __name__ == '__main__':
     dp.run_polling(bot)
