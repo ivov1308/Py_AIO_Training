672b03baf5373c4e8085f19526a4686a76260a57
Ivan Ovchinikov <ivan13air@mail.ru>
2024-10-08 08:23:32 +0300

    Echo: change media-handlers to send_copy

--- Echo_bot.py
+++ Echo_bot.py
@@ -1,4 +1,4 @@
-from aiogram import Bot, Dispatcher, F
+from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
 import config
@@ -10,11 +10,13 @@ dp = Dispatcher()
 
 
 # Этот хэндлер будет срабатывать на команду "/start"
+@dp.message(Command(commands='start'))
 async def process_start_command(message: Message):
     await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
 
 
 # Этот хэндлер будет срабатывать на команду "/help"
+@dp.message(Command(commands='help'))
 async def process_help_command(message: Message):
     await message.answer(
         'Напиши мне что-нибудь и в ответ '
@@ -22,68 +24,18 @@ async def process_help_command(message: Message):
     )
 
 
-# Эти хэндлеры будут срабатывать на отправку различных медиа-файлов
-async def send_photo_echo(message: Message):
-    if message.photo:
-        print("photo")
-        await message.reply_photo(message.photo[0].file_id)
-
-
-async def send_audio_echo(message: Message):
-    if message.audio:
-        print("audio")
-        await message.reply_audio(message.audio.file_id)
-
-
-async def send_video_echo(message: Message):
-    if message.video:
-        print("video")
-        await message.reply_video(message.video.file_id)
-
-
-async def send_sticker_echo(message: Message):
-    if message.sticker:
-        print("sticker")
-        await message.reply_sticker(message.sticker.file_id)
-
-
-@dp.message(F.animation)
-async def send_animation_echo(message: Message):
-    if message.animation:
-        print("animation")
-        await message.reply_animation(message.animation.file_id)
-
-
-@dp.message(F.document)
-async def send_document_echo(message: Message):
-    if message.document:
-        print("document")
-        await message.reply_document(message.document.file_id)
-
-
-@dp.message(F.voice)
-async def send_voice_echo(message: Message):
-    if message.voice:
-        print("voice")
-        await message.reply_voice(message.voice.file_id)
-
-
 # Этот хэндлер будет срабатывать на любые текстовые сообщения
 # кроме команд "/start" и "/help"
+@dp.message()
 async def send_echo(message: Message):
-    if message.text:
-        print("text")
-        await message.reply(text=message.text)
-
-
-# Регистрируем хэндлеры
-dp.message.register(process_start_command, Command(commands='start'))
-dp.message.register(process_help_command, Command(commands='help'))
-dp.message.register(send_photo_echo, F.photo)
-dp.message.register(send_audio_echo, F.audio)
-dp.message.register(send_video_echo, F.video)
-dp.message.register(send_sticker_echo, F.sticker)
-dp.message.register(send_echo)
+    try:
+        print(message.model_dump_json(indent=4, exclude_none=True))
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.reply(
+            text='Данный тип апдейтов не поддерживается '
+                 'методом send_copy'
+        )
 
 
 if __name__ == '__main__':
